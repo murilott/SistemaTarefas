c7219652b65051ddc6fff608fc67032581df0a6a
murilott <murilo_teichert@hotmail.com>
2024-09-25 19:59:28 -0300

    adicionado mais métodos nos controles

--- src/main/java/br/edu/univille/br/relacionamentos/controller/PerfilController.java
+++ src/main/java/br/edu/univille/br/relacionamentos/controller/PerfilController.java
@@ -1,15 +1,13 @@
 package br.edu.univille.br.relacionamentos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import br.edu.univille.br.relacionamentos.entities.Perfil;
 import br.edu.univille.br.relacionamentos.entities.Usuario;
 import br.edu.univille.br.relacionamentos.service.PerfilService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -23,5 +21,14 @@ public class PerfilController {
     public Perfil obter(@RequestBody Usuario usuario) {
         return service.Obter(usuario);
     }
-    
+
+    @PutMapping()
+    public ResponseEntity<?> atualizar(@RequestBody Perfil perfil){
+        try {
+            perfil = service.Atualizar(perfil);
+            return new ResponseEntity<>(perfil, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
 }
